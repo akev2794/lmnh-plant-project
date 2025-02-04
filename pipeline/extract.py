af1286c63bfb167f60b74f5e340b247fdf0fae6c
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2025-02-04 17:03:52 +0000

    extracting

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -3,29 +3,34 @@ This script is for extracting the LMNH raw data from the LMNH api.
 It will request the data from the api, and return a pandas dataframe.
 """
 
+# Native imports
 import asyncio
-from aiohttp import ClientSession
-import pandas as pd
 import json
+
+# Third-party imports
+import pandas as pd
+from aiohttp import ClientSession
 
 
 async def fetch(session: ClientSession, url):
+    """Async function to """
     async with session.get(url) as response:
         return await response.json()
 
 
 async def fetch_all():
+    """ """
     async with ClientSession() as session:
         results = await asyncio.gather(*[fetch(session, f'https://data-eng-plants-api.herokuapp.com/plants/{i}') for i in range(1,51)], return_exceptions=True)
         return results
 
 
-def create_dataframe(data: list[dict]) -> pd.DataFrame:
-    pass
+def create_dataframe() -> pd.DataFrame:
+    """Main script function. Pulls the data from the API and loads it into a DataFrame."""
+    data = asyncio.run(fetch_all())
+    return pd.DataFrame(data)
 
 
 if __name__ == "__main__":
-    data = asyncio.run(fetch_all())
-    print(data)
-    with open('plant_data.json', 'w', encoding="UTF-8") as file:
-        json.dump(data, file, indent=4)
+    raw_extract_dataframe = create_dataframe()
+    print(raw_extract_dataframe)
