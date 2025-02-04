c930cb5545d38002416b3b0bcf75f017c697f425
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2025-02-04 12:31:51 +0000

    added files and file descriptions

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,4 +1,4 @@
 """
 This script is for extracting the LMNH raw data from the LMNH api.
-It will request the data from the api, and return a ...
+It will request the data from the api, and return a pandas dataframe.
 """
