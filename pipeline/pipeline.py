de12e951063a4eda073d2a5698a292c00644dfa8
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2025-02-04 18:05:51 +0000

    pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -3,3 +3,19 @@ This script is for the ETL process for the LMNH plant data.
 The file swill pull data for the API, clean it
 and upload it to the AWS relational database.
 """
+
+# Native imports
+from os import environ as ENV
+
+# Third-party imports
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Local imports
+from extract import create_dataframe
+# from transform ...
+
+
+
+if __name__ == "__main__":
+    conn = create_engine
