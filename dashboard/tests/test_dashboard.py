6f0f16a2ad3a94957c233b302ada35ad41fa13e5
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2025-02-04 12:28:26 +0000

    added files

---
+++ dashboard/tests/test_dashboard.py
@@ -0,0 +1,1 @@
+"""Testing for dashboard.py"""
--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,5 +1,5 @@
 """
-This script is for the creation of a streamlit dashboard.
-It queries an AWS relational database to obtain the relevant LMNH plant data,
-then creates a dashboard based on the wireframe provided in the folder. 
+This script is for the ETL process for the LMNH plant data.
+The file swill pull data for the API, clean it
+and upload it to the AWS relational database.
 """
