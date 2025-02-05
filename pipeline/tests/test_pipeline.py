dc8dd16dbad7f0fb54f596a55c08ff92d1da1cb5
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2025-02-05 14:22:53 +0000

    tests

--- pipeline/tests/test_pipeline.py
+++ pipeline/tests/test_pipeline.py
@@ -2,8 +2,12 @@
 """Testing for pipeline.py"""
 
 # Native imports
+from datetime import datetime
+from unittest.mock import patch, MagicMock
 
-# Third-party imports 
+# Third-party imports
+import pandas as pd
+import pytest
 
 # Local modules 
 from pipeline import load
@@ -13,3 +17,14 @@ What to test:
 load
 
 """
+
+VALID_DATA = [{"plant_id": 3, "soil_moisture": 11, "temperature": 10, "taken_at":datetime.now()}]
+VALID_DF = pd.DataFrame(VALID_DATA)
+
+
+@patch('pipeline.pd.DataFrame.to_sql')
+def test_load_invalid_name(mock_to_sql):
+    # tests for valid name
+    invalid_name = 'hello'
+    with pytest.raises(ValueError):
+        load(invalid_name, VALID_DF, 'engine')
