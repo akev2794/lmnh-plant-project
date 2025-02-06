9fcd49d25267bce7412439f18e5d5ee40d7235bc
Aren <111581520+akev2794@users.noreply.github.com>
2025-02-06 12:19:18 +0000

    Completed basic lambda pipeline

--- terraform/daily_upload.tf
+++ terraform/daily_upload.tf
@@ -1,27 +1,27 @@
 
 
-# ECR Definition for Daily Uploading
+# # ECR Definition for Daily Uploading
 
-resource "aws_ecr_repository" "c15-incitatus-daily-upload-ecr" {
-  name = ""
-}
+# resource "aws_ecr_repository" "c15-incitatus-daily-upload-ecr" {
+#   name = ""
+# }
 
-# Lambda Function for Daily Uploading
+# # Lambda Function for Daily Uploading
 
-resource "aws_lambda_function" "c15-incitatus-daily-upload-lambda-function" {
-  role = ""
-  function_name = ""
-}
+# resource "aws_lambda_function" "c15-incitatus-daily-upload-lambda-function" {
+#   role = ""
+#   function_name = ""
+# }
 
-# EventBridge Scheduler to run daily
+# # EventBridge Scheduler to run daily
 
-resource "aws_scheduler_schedule" "c15-incitatus-daily-upload-schedule" {
-  schedule_expression = "rate(1 day)"
-  flexible_time_window {
-    mode = "OFF"
-  }
-  target {
-    arn      = ""
-    role_arn = ""
-  }
-}
+# resource "aws_scheduler_schedule" "c15-incitatus-daily-upload-schedule" {
+#   schedule_expression = "rate(1 day)"
+#   flexible_time_window {
+#     mode = "OFF"
+#   }
+#   target {
+#     arn      = ""
+#     role_arn = ""
+#   }
+# }
--- terraform/dashboard.tf
+++ terraform/dashboard.tf
@@ -1,10 +1,10 @@
 
 
-# ECS Task Definition to run the dashboard
+# # ECS Task Definition to run the dashboard
 
-resource "aws_ecs_task_definition" "c15-incitatus-dashboard-task-definition" {
-    family = ""
-    container_definitions = jsonencode([{
+# resource "aws_ecs_task_definition" "c15-incitatus-dashboard-task-definition" {
+#     family = ""
+#     container_definitions = jsonencode([{
 
-    }])
-}
+#     }])
+# }
