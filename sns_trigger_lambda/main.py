4ed28249db753da0efd1e96744a9c7d8fd738de1
Joana <nionciu@gmail.com>
2025-02-06 14:10:07 +0000

    Changes connection type

--- sns_trigger_lambda/main.py
+++ sns_trigger_lambda/main.py
@@ -4,22 +4,20 @@ import pandas as pd
 from sqlalchemy import create_engine, Engine
 from sqlalchemy.engine import URL
 from dotenv import load_dotenv
+import pyodbc
 
 
 def make_engine():
     """Creates sqlalchemy engine."""
-    connection_string = f"""
+    conn = pyodbc.connect(f"""
         DRIVER={{ODBC Driver 18 for SQL Server}};
         SERVER={ENV["DB_HOST"]},{ENV["DB_PORT"]};
         DATABASE={ENV["DB_NAME"]};
         UID={ENV["DB_USER"]};
         PWD={ENV["DB_PASSWORD"]};
-        TrustServerCertificate=Yes"""
-
-    connection_url = URL.create(
-        "mssql+pyodbc", query={"odbc_connect": connection_string}
-    )
-    return create_engine(connection_url)
+        TrustServerCertificate=yes;
+    """)
+    return conn
 
 
 def get_data():
@@ -43,33 +41,61 @@ FilteredReadings AS (
         plant_id,
         soil_moisture,
         temperature,
+        taken_at,
         min_temp,
         max_temp,
         min_moisture,
         max_moisture
     FROM LastThreeReadings
     WHERE rn <= 3
-    )
+),
+OutOfRangeReadings AS (
     SELECT 
+        plant_id,
+        taken_at,
+        CASE
+            WHEN temperature < min_temp OR temperature > max_temp THEN 'Temperature out of range'
+            ELSE NULL
+        END AS temperature_status,
+        CASE
+            WHEN soil_moisture < min_moisture OR soil_moisture > max_moisture THEN 'Moisture out of range'
+            ELSE NULL
+        END AS moisture_status
+    FROM FilteredReadings
+)
+SELECT 
     plant_id,
     CASE
-        WHEN SUM(CASE WHEN temperature < min_temp THEN 1 WHEN temperature > max_temp THEN 1 ELSE 0 END) > 1 THEN 'Temperature out of range'
+        WHEN SUM(CASE WHEN temperature_status IS NOT NULL THEN 1 ELSE 0 END) > 1 THEN 'Temperature out of range'
         ELSE 'Temperature within range'
     END AS temperature_status,
-
     CASE
-        WHEN SUM(CASE WHEN soil_moisture < min_moisture THEN 1 WHEN soil_moisture > max_moisture THEN 1 ELSE 0 END) > 1 THEN 'Moisture out of range'
+        WHEN SUM(CASE WHEN moisture_status IS NOT NULL THEN 1 ELSE 0 END) > 1 THEN 'Moisture out of range'
         ELSE 'Moisture within range'
-    END AS moisture_status
-    FROM FilteredReadings
-    GROUP BY plant_id
-    ORDER BY plant_id;
+    END AS moisture_status,
+    MAX(CASE 
+            WHEN temperature_status IS NOT NULL OR moisture_status IS NOT NULL 
+            THEN taken_at
+            ELSE NULL
+        END) AS last_out_of_range_time
+FROM OutOfRangeReadings
+GROUP BY plant_id
+ORDER BY plant_id;
     """
-    engine = make_engine()
-    with engine.connect() as connection:
-        result = connection.execute(query)
-    for row in result:
+    conn = make_engine()
+    cursor = conn.cursor()
+
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
+    cursor.close()
+    conn.close()
+
+
+def insert_incidents():
+    ...
 
 
 if __name__ == "__main__":
