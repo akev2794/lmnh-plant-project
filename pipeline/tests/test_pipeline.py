dff38eb89f54aada1f55c975105762546988aa0f
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2025-02-07 15:35:10 +0000

    tests

--- pipeline/tests/test_pipeline.py
+++ pipeline/tests/test_pipeline.py
@@ -4,6 +4,7 @@
 
 # Native imports
 from unittest.mock import patch
+from os import environ as ENV
 
 # Third-party imports
 import pytest
@@ -12,9 +13,15 @@ import pyodbc
 # Local modules 
 from pipeline import create_env_connection
 
-
+@patch.dict(ENV, {
+    'DB_HOST': 'localhost',
+    'DB_PORT': '1433',
+    'DB_NAME': 'test_db',
+    'DB_USER': 'user',
+    'DB_PASSWORD': 'password'
+})
 @patch('pyodbc.connect')
-def test_create_env_connection_invalid( mock_connect):
+def test_create_env_connection_invalid(mock_connect):
     mock_connect.side_effect = pyodbc.Error("Error connecting to database:")
     with pytest.raises(pyodbc.Error, match="Error connecting to database:"):
         create_env_connection()
