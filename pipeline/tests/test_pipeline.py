7059b768c247d801ad9b58d0ddc70421ee2e5eac
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2025-02-07 15:31:51 +0000

    tests

--- pipeline/tests/test_pipeline.py
+++ pipeline/tests/test_pipeline.py
@@ -3,32 +3,19 @@
 """Testing for pipeline.py"""
 
 # Native imports
-from datetime import datetime
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 
 # Third-party imports
-import pandas as pd
 import pytest
 import pyodbc
 
 # Local modules 
-from pipeline import load_to_recording
+from pipeline import create_env_connection
 
 
-### Load to recording
-# @patch('pyodbc.connect')  # Mock pyodbc.connect, which returns a mock connection
-# def test_load_to_recording_invalid_df(mock_connect):
-#     mock_conn = MagicMock()
-#     mock_conn.side_effect = pyodbc.Error("Mocked pyodbc error")
-#     mock_connect.return_value = mock_conn
-#     data = [1]
-#     with pytest.raises(pyodbc.Error):
-#         load_to_recording(data, mock_conn)
-
-
-# @patch('pyodbc.Connection')
-# def test_load_to_recording_valid_df(mock_conn):
-#     mock_conn = MagicMock()
-#     valid_data= [(1, 2, 3 ,4, 5)]
-#     assert load_to_recording(valid_data, conn) == None
+@patch('pyodbc.connect')
+def test_create_env_connection_invalid( mock_connect):
+    mock_connect.side_effect = pyodbc.Error("Error connecting to database:")
+    with pytest.raises(pyodbc.Error, match="Error connecting to database:"):
+        create_env_connection()
 
